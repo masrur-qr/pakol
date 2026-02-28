16722a8f8b184a8d44871f063da9a71c28bad25e
masrur <masrur.gdsc@gmail.com>
2026-02-28 11:47:18 +0500

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from blog.models import Post, Comment
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = (
+            'author',
+            # 'user',
+            'owner', 
+            'text',
+        )
